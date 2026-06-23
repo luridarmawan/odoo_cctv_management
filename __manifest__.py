8c0bd676c7f98a48d49dd8698a8c4b5fd336fdf7
luridarmawan <luri@carik.id>
2026-06-23 12:03:08 +0700

    add administrator to group CCTV Administratr

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "CCTV Management",
-    "version": "19.0.1.4.0",
+    "version": "19.0.1.5.0",
     "summary": "Manage CCTV infrastructure (NVR/DVR, cameras)",
     "description": """
         CCTV Management Module for Odoo 19.
